63e16812c26a97f99cf75e4a4657f4a851526e2d
shubhamwrathcode <shubhamwrathcode@gmail.com>
2026-09-17 17:53:21 +0530

    Fix file casing in ImageAssets.js for Metro bundler resolution

--- src/helper/ImageAssets.js
+++ src/helper/ImageAssets.js
@@ -453,8 +453,8 @@ export const landingpagedemo = require('../../assets/images/landingpagedemo.png'
 export const stakingHomeIcon = require('../../assets/images/stakingHomeIcon.png');
 export const newsImg = require('../../assets/images/newsImg.png');
 export const EarnIcon = require('../../assets/images/EarnIcon.png');
-export const MarketIcon = require('../../assets/images/MarketIcon.png');
-export const TradeIcon = require('../../assets/images/TradeIcon.png');
+export const MarketIcon = require('../../assets/images/marketIcon.png');
+export const TradeIcon = require('../../assets/images/tradeIcon.png');
 export const AssetsIcon = require('../../assets/images/AssetsIcon.png');
 
 
